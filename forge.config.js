fe2ca00a178002f1abd06636b1eef5e62a1e1418
liforra <git@liforra.de>
2025-09-04 11:42:22 +0200

    Fixed Workflow

--- forge.config.js
+++ forge.config.js
@@ -4,30 +4,62 @@ const { FuseV1Options, FuseVersion } = require("@electron/fuses");
 module.exports = {
 	packagerConfig: {
 		asar: true,
+		executableName: "liforratest",
+		icon: "./icon", // base name, Forge will find .ico/.icns/.png
 	},
 	rebuildConfig: {},
 	makers: [
+		// Windows only
 		{
 			name: "@electron-forge/maker-squirrel",
-			config: {},
-		},
-		{
-			name: "@electron-forge/maker-zip",
-			platforms: ["darwin"],
-		},
-		{
-			name: "@electron-forge/maker-deb",
-			config: {},
-		},
-		{
-			name: "@electron-forge/maker-squirrel",
+			platforms: ["win32"],
 			config: {
+				name: "liforratest",
+				setupExe: "liforratest-Setup-${version}.exe",
+				noMsi: true, // disable MSI to reduce file conflicts
 				//certificateFile: "./cert.pfx",
 				//certificatePassword: process.env.CERTIFICATE_PASSWORD,
 			},
 		},
+		// macOS only
+		{
+			name: "@electron-forge/maker-zip",
+			platforms: ["darwin"],
+		},
+		// Linux only
+		{
+			name: "@electron-forge/maker-deb",
+			platforms: ["linux"],
+			config: {},
+		},
+		{
+			name: "@electron-forge/maker-rpm",
+			platforms: ["linux"],
+			config: {},
+		},
+		{
+			name: "@electron-forge/maker-flatpak",
+			platforms: ["linux"],
+			config: {
+				options: {
+					id: "de.liforra.liforratest",
+					runtime: "org.freedesktop.Platform",
+					runtimeVersion: "23.08",
+					sdk: "org.freedesktop.Sdk",
+					command: "liforratest",
+					icon: "./icon.png",
+					finishArgs: [
+						"--share=network",
+						"--socket=fallback-x11",
+						"--socket=wayland",
+						"--device=dri",
+					],
+				},
+			},
+		},
 		{
 			name: "@reforged/maker-appimage",
+			platforms: ["linux"],
 			config: {
 				options: {
 					categories: ["Network"],
@@ -41,8 +73,6 @@ module.exports = {
 			name: "@electron-forge/plugin-auto-unpack-natives",
 			config: {},
 		},
-		// Fuses are used to enable/disable various Electron functionality
-		// at package time, before code signing the application
 		new FusesPlugin({
 			version: FuseVersion.V1,
 			[FuseV1Options.RunAsNode]: false,
